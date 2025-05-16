0191bdac14ce451d6ab829bb097202620095481e
eunKyeol <115553490+ekkkabi@users.noreply.github.com>
2025-05-17 00:21:43 +0900

    [level 2] Title: 피보나치 수, Time: 2.89 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let dp = new Array(n+1).fill(0)
+    dp[0] = 0, dp[1] = 1
+    for (let i = 2; i<=n; i++){
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567
+    }
+    return dp[n]
+}
